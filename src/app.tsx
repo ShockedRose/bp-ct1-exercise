e9f5918618e483fdf455e1e68ad574ce60f7c0b1
Miguel R <miguel.rosa@devsu.com>
2024-01-05 10:58:33 -0500

    partial design system impl, incompatibility

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,5 @@
 import "./app.scss";
+import "@pichincha/ds-core/dist/ds-core/ds-core.css";
 
 import LogoBP from "./assets/logo-BP.svg";
 
@@ -6,6 +7,8 @@ function App() {
 	return (
 		<main>
 			<img src={LogoBP} alt="logo banco pichincha" />
+
+			<div></div>
 		</main>
 	);
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,14 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: "jsdom",
-    setupFiles: ["setup.ts"],
-    testMatch: ["./src/**/*.test.tsx", "./src/**/*.test.ts"],
-    globals: true,
-  },
+	plugins: [react()],
+	test: {
+		environment: "jsdom",
+		setupFiles: ["setup.ts"],
+		testMatch: ["./src/**/*.test.tsx", "./src/**/*.test.ts"],
+		globals: true,
+	},
+	optimizeDeps: {
+		exclude: ["js-big-decimal"],
+	},
 } as UserConfigExport);
